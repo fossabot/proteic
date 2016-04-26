26e60a514a41283a0069cd5bc44fffb21fc1c6f5
Jorge Yagüe París <jorge.yague@treelogic.com>
2016-04-26 14:00:32 +0200

    Add some linechart tests

--- test/test.charts.linechart.js
+++ test/test.charts.linechart.js
@@ -1,3 +1,18 @@
+describe('SvgLinechartStrategy', () => {
+  describe('_loadConfigOnContext(config)', () => {
+    it('should apply the default configuration if a custom one is ommited', () => {
+      var data = [{ x: 0, y: 1 }, { x: 0, y: 2 }];
+      var width = 250, height = 100;
+      var config = { width: width, height: height };
+      
+      var svg = new SvgLinechartStrategy(data, config, 'Linechart');
+      var result = svg._loadConfigOnContext(config);
+      result.should.have.property('width').equals(width);
+      result.should.have.property('height').equals(height);
+    });
+  });
+});
+
 describe('Linechart', () => {
   beforeEach(() => {
     //Append default chart div
@@ -25,13 +40,20 @@ describe('Linechart', () => {
       assert.isOk(chart);
     });
 
+    it('should construct a line chart with some custom configuration', () => {
+      var data = [{ x: 0, y: 1 }, { x: 0, y: 2 }];
+      var width = 250, height = 100;
+      var config = { width: width, height: height };
+      var chart = new Linechart(data, config);
+      assert.isOk(chart);
+    });
+
     it('throws a "Wrong data format" TypeError if data is not an object neither an array', () => {
       var data = 'wrong parameter';
       assert.throws(() => {
         new Linechart(data)
       }, TypeError, 'Wrong data format');
     });
-
   });
 
   describe('chart functions', () => {
