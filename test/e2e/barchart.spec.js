9229551a338f190a79bdb37db1b144b7c42f408e
0xNacho <nachogarcia91@gmail.com>
2016-05-10 08:54:58 +0200

    add e2e tests. Closes #24

--- test/e2e/barchart.spec.js
+++ test/e2e/barchart.spec.js
@@ -1,8 +1,15 @@
 browser.ignoreSynchronization = true;
 
 describe('Simple barchart', () => {
-  it('should match the empty title', () => {
-    browser.driver.get('http://localhost:9000/examples/simple_barchart.html');
-    expect(browser.getTitle()).toEqual('Simple barchart');
-  }); 
+    it('should match the empty title', () => {
+        browser.driver.get('http://localhost:9000/examples/simple_barchart.html');
+        expect(browser.getTitle()).toEqual('Simple barchart');
+    });
+
+    it('should have a correct number of elemenets', () => {
+        browser.executeScript('return barchart').then(function (barchart) {
+            expect(12).toEqual(barchart.data.length);
+        });
+    });
+
 });
--- test/e2e/linechart.spec.js
+++ test/e2e/linechart.spec.js
@@ -4,5 +4,12 @@ describe('Simple linechart', () => {
   it('should match the title', () => {
     browser.driver.get('http://localhost:9000/examples/simple_linechart.html');
     expect(browser.getTitle()).toEqual('Simple linechart');
-  }); 
+  });
+
+  it('should have a correct number of elemenets', () => {
+    browser.executeScript('return linechart').then(function (linechart) {
+      expect(12).toEqual(linechart.data.length);
+    });
+  });
+
 });
