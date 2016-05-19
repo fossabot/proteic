57c0923221d75729f43b9802334aed0510bf3222
0xNacho <nachogarcia91@gmail.com>
2016-05-19 15:12:27 +0200

    add factory.js

---
+++ src/utils/factory.js
@@ -0,0 +1,16 @@
+(() => {
+    window['ProteusFactory'] = {
+        create(params) {
+            switch (params.type) {
+                case 'Linechart':
+                    return new Linechart(params.data, params.config);
+                case 'Barchart':
+                    return new Barchart(params.data, params.config);
+                case 'Gauge':
+                    return new Gauge(params.data, params.config);
+                case 'Streamgraph':
+                    return new Streamgraph(params.data, params.config);
+            }
+        }
+    }
+})();
