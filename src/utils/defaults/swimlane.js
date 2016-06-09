c8ebf15657007f27c1b9b9f03fe1a2239165e4ed
0xNacho <nachogarcia91@gmail.com>
2016-06-09 09:51:38 +0200

    Remove globals.js and add one default file per chart. #58

---
+++ src/utils/defaults/swimlane.js
@@ -0,0 +1,31 @@
+var _default = _default || {};
+
+_default.Swimlane = {
+    selector: '#chart',
+    margin: {
+        top: 20,
+        right: 20,
+        bottom: 30,
+        left: 50
+    },
+    width: '99%', // %, auto, or numeric
+    height: 550,
+    events: {
+        down() {
+
+        },
+        over() {
+
+        },
+        up() {
+
+        },
+        click() {
+
+        },
+        leave() {
+
+        }
+
+    }
+};
