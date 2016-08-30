6040ad1df04fc06adf9f76c5fe8a00f5b2b337e4
0xNacho <nachogarcia91@gmail.com>
2016-08-30 08:32:29 +0200

    some minor changes on swimlane

--- src/svg/strategy_swimlane.js
+++ src/svg/strategy_swimlane.js
@@ -14,16 +14,16 @@ class SvgSwimlaneStrategy {
     this.miniHeight = this.lanes.length * 12 + 50
     this.mainHeight = this.height - this.miniHeight - 50;
 
-    var dMin = d3.time.sunday(d3.min(this.items, (d) => new Date(d.start)))
+    var dMin = d3.timeSunday(d3.min(this.items, (d) => new Date(d.start)))
     var dMax = d3.max(this.items, (d) => new Date(d.end));
-    
-    this.now = new Date();
-    
-    this.x = d3.time.scale().domain([dMin, dMax]).range([0, this.width]);
-    this.x1 = d3.time.scale().range([0, this.width]);
+
+    this.now = new Date(data.items[0].start);
+
+    this.x = d3.scaleTime().domain([dMin, dMax]).range([0, this.width]);
+    this.x1 = d3.scaleTime().range([0, this.width]);
     this.ext = d3.extent(this.lanes, (d) => d.id);
-    this.y1 = d3.scale.linear().domain([this.ext[0], this.ext[1] + 1]).range([0, this.mainHeight]);
-    this.y2 = d3.scale.linear().domain([this.ext[0], this.ext[1] + 1]).range([0, this.miniHeight]);
+    this.y1 = d3.scaleLinear().domain([this.ext[0], this.ext[1] + 1]).range([0, this.mainHeight]);
+    this.y2 = d3.scaleLinear().domain([this.ext[0], this.ext[1] + 1]).range([0, this.miniHeight]);
 
     if (!this._initialized) {
       this._initialize();
@@ -33,14 +33,18 @@ class SvgSwimlaneStrategy {
     this.itemRects = this.main.append('g')
       .attr('clip-path', 'url(#clip)');
 
-    this.mini.append('g').selectAll('miniItems')
-      .data(this.getPaths(this.items))
-      .enter().append('path')
-      .attr('class', (d) => 'miniItem ' + d.class)
-      .attr('d', (d) => d.path);
 
+    /**
+        this.mini.append('g').selectAll('miniItems')
+          .data(this.getPaths(this.items))
+          .enter().append('path')
+          .attr('class', (d) => 'miniItem ' + d.class)
+          .attr('d', (d) => d.path);
+    **/
     var self = this;
     // invisible hit area to move around the selection window
+
+    /**
     this.mini.append('rect')
       .attr('pointer-events', 'painted')
       .attr('width', this.width)
@@ -49,29 +53,40 @@ class SvgSwimlaneStrategy {
       .on('mouseup', function () {
         var origin = d3.mouse(this);
         var point = self.x.invert(origin[0]);
+                        
+        console.log(d3.brushSelection());
+        
         var halfExtent = (self.brush.extent()[1].getTime() - self.brush.extent()[0].getTime()) / 2;
         var start = new Date(point.getTime() - halfExtent)
         var end = new Date(point.getTime() + halfExtent);
+        
+        
+        console.log('point', point, 'half', halfExtent, 'start', start, 'end', end);
+        
+        
         self.brush.extent([start, end]);
         self.display(self);
       });
-
+**/
     // draw the selection area
     var self = this;
-    this.brush = d3.svg.brush()
-      .x(this.x)
-      .extent([d3.time.monday(this.now), d3.time.saturday.ceil(this.now)])
+    this.brush = d3.brushX()
+      // .extent([d3.timeMonday(this.now), d3.timeSaturday.ceil(this.now)])
+      .extent([[0, 0], [this.width, this.height]])
+
       .on("brush", () => this.display(this));
-
+    /*
     this.mini.append('g')
-      .attr('class', 'x brush')
-      .call(this.brush)
-      .selectAll('rect')
-      .attr('y', 1)
-      .attr('height', this.miniHeight - 1);
+    .attr('class', 'x brush')
+    .call(this.brush)
+    .selectAll('rect')
+    .attr('y', 1)
+    .attr('height', this.miniHeight - 1);
 
-    this.mini.selectAll('rect.background').remove();
-    this.display(self);
+  this.mini.selectAll('rect.background').remove();
+  
+  this.display(self);
+  **/
 
 
   }
@@ -95,40 +110,84 @@ class SvgSwimlaneStrategy {
       .attr('height', this.mainHeight)
       .attr('class', 'main');
 
-    this.mini = this.svg.append('g')
-      .attr('transform', 'translate(' + this.margin.left + ',' + (this.mainHeight + 60) + ')')
-      .attr('width', this.width)
-      .attr('height', this.miniHeight)
-      .attr('class', 'mini');
+
+
+
+
+
+
+
+
+
+
+
+
+
+    this.mybrush = d3.brushX().extent([[0, 0], [this.width, 100]])
+      .on("end", this.brushended);
+
+
+    this.mini = this.svg.append("g")
+      .attr("class", "axis axis--grid")
+      .attr("transform", "translate(0," + 100 + ")")
+      .call(d3.axisBottom(this.x)
+        .ticks(d3.timeMonth, 12)
+        .tickSize(-100)
+        .tickFormat(function () { return null; }))
+      .selectAll(".tick")
+      .classed("tick--minor", function (d) { console.log(d); return d.getHours(); });
+
+    this.mini.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + 100 + ")")
+      .call(d3.axisBottom(this.x)
+        .ticks(d3.timeYear)
+        .tickPadding(0))
+      .attr("text-anchor", null)
+      .selectAll("text")
+      .attr("x", 6);
+
+    this.mini.append("g")
+      .attr("class", "brush")
+      .call(this.mybrush);
+
+
+
+
+
+    /**
+        this.mini = this.svg.append('g')
+          .attr('transform', 'translate(' + this.margin.left + ',' + (this.mainHeight + 60) + ')')
+          .attr('width', this.width)
+          .attr('height', this.miniHeight)
+          .attr('class', 'mini');
+          **/
+
     this._initialized = true;
 
     // draw the x axis
-    this.xDateAxis = d3.svg.axis()
+    this.xDateAxis = d3.axisBottom()
       .scale(this.x)
-      .orient('bottom')
-      .ticks(d3.time.mondays, (this.x.domain()[1] - this.x.domain()[0]) > 15552e6 ? 2 : 1)
-      .tickFormat(d3.time.format('%d'))
+      .ticks(d3.timeMonday, (this.x.domain()[1] - this.x.domain()[0]) > 15552e6 ? 2 : 1)
+      .tickFormat(d3.timeFormat('%d'))
       .tickSize(6, 0, 0);
 
-    this.x1DateAxis = d3.svg.axis()
+    this.x1DateAxis = d3.axisBottom()
       .scale(this.x1)
-      .orient('bottom')
-      .ticks(d3.time.days, 1)
-      .tickFormat(d3.time.format('%a %d'))
+      .ticks(d3.timeDay, 1)
+      .tickFormat(d3.timeFormat('%a %d'))
       .tickSize(6, 0, 0);
 
-    this.xMonthAxis = d3.svg.axis()
+    this.xMonthAxis = d3.axisTop()
       .scale(this.x)
-      .orient('top')
-      .ticks(d3.time.months, 1)
-      .tickFormat(d3.time.format('%b %Y'))
+      .ticks(d3.timeMonth, 1)
+      .tickFormat(d3.timeFormat('%b %Y'))
       .tickSize(15, 0, 0);
 
-    this.x1MonthAxis = d3.svg.axis()
+    this.x1MonthAxis = d3.axisTop()
       .scale(this.x1)
-      .orient('top')
-      .ticks(d3.time.mondays, 1)
-      .tickFormat(d3.time.format('%b - Week %W'))
+      .ticks(d3.timeMonday, 1)
+      .tickFormat(d3.timeFormat('%b - Week %W'))
       .tickSize(15, 0, 0);
 
     this.main.append('g')
@@ -144,27 +203,28 @@ class SvgSwimlaneStrategy {
       .attr('dx', 5)
       .attr('dy', 12);
 
-    this.mini.append('g')
-      .attr('transform', 'translate(0,' + this.miniHeight + ')')
-      .attr('class', 'axis date')
-      .call(this.xDateAxis);
-
-    this.mini.append('g')
-      .attr('transform', 'translate(0,0.5)')
-      .attr('class', 'axis month')
-      .call(this.xMonthAxis)
-      .selectAll('text')
-      .attr('dx', 5)
-      .attr('dy', 12);
-      
+    /**
+        this.mini.append('g')
+          .attr('transform', 'translate(0,' + this.miniHeight + ')')
+          .attr('class', 'axis date')
+          .call(this.xDateAxis);
+    
+        this.mini.append('g')
+          .attr('transform', 'translate(0,0.5)')
+          .attr('class', 'axis month')
+          .call(this.xMonthAxis)
+          .selectAll('text')
+          .attr('dx', 5)
+          .attr('dy', 12);
+          **/
     // draw the lanes for the main chart
     this.main.append('g').selectAll('.laneLines')
       .data(this.lanes)
       .enter().append('line')
       .attr('x1', 0)
-      .attr('y1', (d) => d3.round(this.y1(d.id)) + 0.5)
+      .attr('y1', (d) => d3.format('d')(this.y1(d.id)) + 0.5)
       .attr('x2', this.width)
-      .attr('y2', (d) => d3.round(this.y1(d.id)) + 0.5)
+      .attr('y2', (d) => d3.format('d')(this.y1(d.id)) + 0.5)
       .attr('stroke', (d) => d.label === '' ? 'white' : 'lightgray');
 
     this.main.append('g').selectAll('.laneText')
@@ -178,14 +238,15 @@ class SvgSwimlaneStrategy {
       .attr('class', 'laneText');
 
     // draw the lanes for the mini chart
+    /**
     this.mini.append('g')
       .selectAll('.laneLines')
       .data(this.lanes)
       .enter().append('line')
       .attr('x1', 0)
-      .attr('y1', (d) => d3.round(this.y2(d.id)) + 0.5)
+      .attr('y1', (d) => d3.format('d')(this.y2(d.id)) + 0.5)
       .attr('x2', this.width)
-      .attr('y2', (d) => d3.round(this.y2(d.id)) + 0.5)
+      .attr('y2', (d) => d3.format('d')(this.y2(d.id)) + 0.5)
       .attr('stroke', (d) => d.label === '' ? 'white' : 'lightgray');
 
     this.mini.append('g')
@@ -198,20 +259,47 @@ class SvgSwimlaneStrategy {
       .attr('dy', '0.5ex')
       .attr('text-anchor', 'end')
       .attr('class', 'laneText');
-
+**/
     // draw a line representing today's date
     this.main.append('line')
       .attr('y1', 0)
       .attr('y2', this.mainHeight)
       .attr('class', 'main todayLine')
       .attr('clip-path', 'url(#clip)');
+    /**
+        this.mini.append('line')
+          .attr('x1', this.x(this.now) + 0.5)
+          .attr('y1', 0)
+          .attr('x2', this.x(this.now) + 0.5)
+          .attr('y2', this.miniHeight)
+          .attr('class', 'todayLine');
+          
+          **/
 
-    this.mini.append('line')
-      .attr('x1', this.x(this.now) + 0.5)
-      .attr('y1', 0)
-      .attr('x2', this.x(this.now) + 0.5)
-      .attr('y2', this.miniHeight)
-      .attr('class', 'todayLine');
+  }
+
+
+  brushended() {
+    if (!d3.event.sourceEvent) return; // Only transition after input.
+    if (!d3.event.selection) return; // Ignore empty selections.
+
+    console.log(d3.event.selection);
+    console.log(d3.event.sourceEvent);
+    /**
+    var domain0 = d3.event.selection.map(this.x.invert),
+        domain1 = domain0.map(d3.timeDay.round);
+  
+    // If empty when rounded, use floor & ceil instead.
+    if (domain1[0] >= domain1[1]) {
+      domain1[0] = d3.timeDay.floor(domain0[0]);
+      domain1[1] = d3.timeDay.ceil(domain0[1]);
+    }
+  
+  console.log(domain0, domain1);
+    d3.select(this)
+      .transition()
+        .call(brush.move, domain1.map(this.x));
+        **/
   }
 
 	/**
@@ -255,27 +343,28 @@ class SvgSwimlaneStrategy {
 
   display(self) {
     var rects, labels
-      , minExtent = d3.time.day(this.brush.extent()[0])
-      , maxExtent = d3.time.day(this.brush.extent()[1])
+      , minExtent = d3.timeDay(this.brush.extent()[0])
+      , maxExtent = d3.timeDay(this.brush.extent()[1])
       , visItems = this.items.filter((d) => new Date(d.start) < maxExtent && new Date(d.end) > minExtent);
 
-    this.mini.select('.brush').call(this.brush.extent([minExtent, maxExtent]));
+    //this.mini.select('.brush').call(this.brush.extent([minExtent, maxExtent]));
 
     this.x1.domain([minExtent, maxExtent]);
 
-    if ((maxExtent - minExtent) > 1468800000) {
-      this.x1DateAxis.ticks(d3.time.mondays, 1).tickFormat(d3.time.format('%a %d'))
-      this.x1MonthAxis.ticks(d3.time.mondays, 1).tickFormat(d3.time.format('%b - Week %W'))
-    }
-    else if ((maxExtent - minExtent) > 172800000) {
-      this.x1DateAxis.ticks(d3.time.days, 1).tickFormat(d3.time.format('%a %d'))
-      this.x1MonthAxis.ticks(d3.time.mondays, 1).tickFormat(d3.time.format('%b - Week %W'))
-    }
-    else {
-      this.x1DateAxis.ticks(d3.time.hours, 4).tickFormat(d3.time.format('%I %p'))
-      this.x1MonthAxis.ticks(d3.time.days, 1).tickFormat(d3.time.format('%b %e'))
-    }
-
+    /**
+        if ((maxExtent - minExtent) > 1468800000) {
+          this.x1DateAxis.ticks(d3.timeMonday, 1).tickFormat(d3.timeFormat('%a %d'))
+          this.x1MonthAxis.ticks(d3.timeMonday, 1).tickFormat(d3.timeFormat('%b - Week %W'))
+        }
+        else if ((maxExtent - minExtent) > 172800000) {
+          this.x1DateAxis.ticks(d3.timeDay, 1).tickFormat(d3.timeFormat('%a %d'))
+          this.x1MonthAxis.ticks(d3.timeMonday, 1).tickFormat(d3.timeFormat('%b - Week %W'))
+        }
+        else {
+          this.x1DateAxis.ticks(d3.timeHour, 4).tickFormat(d3.timeFormat('%I %p'))
+          this.x1MonthAxis.ticks(d3.timeDay, 1).tickFormat(d3.timeFormat('%b %e'))
+        }
+    **/
 
     // shift the today line
     this.main.select('.main.todayLine')
