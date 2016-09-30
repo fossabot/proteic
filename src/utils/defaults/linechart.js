50c9b2fe31f397585bf609a36543f85d3e62c40a
0xNacho <nachogarcia91@gmail.com>
2016-09-30 14:55:30 +0200

    Remove invalid config options

--- src/utils/defaults/linechart.js
+++ src/utils/defaults/linechart.js
@@ -2,14 +2,6 @@ import * as Colors from '../colors';
 
 export const defaults = {
     selector: '#chart',
-    xaxis: {
-        label: 'X',
-        ticks: 5
-    },
-    yaxis: {
-        label: 'Y',
-        ticks: 5
-    },
     colorScale: Colors.category7(),
     area: false,
     areaOpacity: 0.4,
@@ -21,38 +13,10 @@ export const defaults = {
     },
     width: '100%', // %, auto, or numeric 
     height: 250,
-    style: {
-        '.line': {
-            'stroke-width': 2,
-            'fill': 'none'
-        },
-        '.area': {
-            'stroke-width': 0
-        },
-        '.axis': {
-            'font': '10px sans-serif'
-        },
-        '.axis path,.axis line': {
-            'fill': 'none',
-            'stroke': '#000',
-            'shape-rendering': 'crispEdge'
-        },
-        '.x.axis path': {
-            'display': 'none'
-        },
-        '.x.axis.label, .y.axis.label': {
-            'font': '12px sans-serif'
-        }
-    },
     markers: {
         shape: 'circle',
         size: 5,
-        color: '#FFFCCA',
-        outlineColor: '#537780',
         outlineWidth: 2
-    },
-    tooltip(data) {
-        return JSON.stringify(data);
     },
     events: {
         down() {
@@ -76,8 +40,6 @@ export const defaults = {
             console.log(d, i);
         }
     },
-    tickLabel: '',
-    transitionDuration: 300,
     maxNumberOfElements: 100, // used by keepDrawing to reduce the number of elements in the current chart
     sortData: {
         descending: true,
