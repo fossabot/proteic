3ad5d8a0a322249f3563158076858f496b09cac6
0xNacho <nachogarcia91@gmail.com>
2016-10-17 09:15:17 +0200

    Add datasource docs

--- src/datasources/Datasource.js
+++ src/datasources/Datasource.js
@@ -1,17 +1,55 @@
-export default class Datasource{
-    constructor () {
+/**
+ * 
+ * A Datasource is the name given to the connection set up to a data endpoint. This class defines the common methods for the datasources,
+ * such as start() and stop().
+ * 
+ * @export Default export: Datasource class
+ * 
+ * @class Datasource The Datasource class
+ * 
+ */
+export default class Datasource {
+    /**
+     * Creates an instance of Datasource.
+     * 
+     * 
+     * @memberOf Datasource
+    
+     */
+    constructor() {
         this.filters = [];
     }
-    
-    start(){
+
+    /**
+     * Starts the stream of data
+     * 
+     * 
+     * @memberOf Datasource
+     */
+    start() {
         console.log('Starting datasource');
     }
+
+    /**
+     * 
+     * If started, this method stops the stream of data
+     * 
+     * @memberOf Datasource
     
-    stop(){
+     */
+    stop() {
         console.log('Stopping datasource');
     }
-    
-    filter (filter){
+
+    /**
+     * Filters the incoming messages. All the data input that do not comply the filter condition will be discarded
+     * 
+     * @param {any} filter A filter condition
+     * @returns this
+     * 
+     * @memberOf Datasource
+     */
+    filter(filter) {
         return this;
     }
 }
