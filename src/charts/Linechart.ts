8a460d86f6ec869f28007a3c8f6600ff15e12309
0xNacho <nachogarcia91@gmail.com>
2016-11-07 13:35:36 +0100

    Add yAxisShow parameter.#84

--- src/charts/Linechart.ts
+++ src/charts/Linechart.ts
@@ -37,6 +37,7 @@ class Linechart extends Chart {
             yAxisType = userData['yAxisType'] || defaults.yAxisType,
             yAxisFormat = userData['yAxisFormat'] || defaults.yAxisFormat,
             yAxisLabel = userData['yAxisLabel'] || defaults.yAxisLabel,
+            yAxisShow = userData['yAxisShow'] || defaults.yAxisShow,
             //Color
             colorScale = userData['colorScale'] || defaults.colorScale,
             //Events
@@ -67,6 +68,7 @@ class Linechart extends Chart {
         config.put('yAxisType', yAxisType);
         config.put('yAxisFormat', yAxisFormat);
         config.put('yAxisLabel', yAxisLabel);
+        config.put('yAxisShow', yAxisShow);
         config.put('colorScale', colorScale);
         config.put('onDown', onDown);
         config.put('onUp', onUp);
