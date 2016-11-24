ed400488d68d96c51398816e732908992ae52de4
Jorge Yagüe París <jorge.yague@treelogic.com>
2016-11-24 11:07:37 +0100

    Update proteic.js.org to new version using Bulma

--- assets/js/charts/gauge.js
+++
@@ -1,4 +0,0 @@
-            var data = [{x: 50}];
-            //Default config uses #chart selector
-            gauge = new Gauge(data, {selector: '#gaugegraph', min: 0, max: 100, numericIndicator: false});
-            gauge.draw();
