faf602d0ea500d112b733ec38f4a86540fa7b851
0xNacho <nachogarcia91@gmail.com>
2017-01-23 17:56:05 +0100

    Finally, tests are working again :tada: :tada:

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-// Karma configuration
-// Generated on Wed Apr 06 2016 14:18:09 GMT+0200 (CEST)
-=======
->>>>>>> webpack
 var webpackConfig = require('./webpack.config');
 
 module.exports = function (config) {
@@ -15,33 +10,13 @@ module.exports = function (config) {
     exclude: [
     ],
     preprocessors: {
-<<<<<<< HEAD
-      'test/**/*.ts': ['webpack']
-    },
-
-=======
       'src/**/!(defaults)/*.js': ['coverage'],
       'test/**/*.ts': ['webpack']
     },
->>>>>>> webpack
     webpack: {
       module: webpackConfig.module,
       resolve: webpackConfig.resolve
     },
-<<<<<<< HEAD
-    reporters: ['progress'],
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: [
-      'progress',
-      // Output code coverage files
-      'coverage'
-    ],
-
-=======
     reporters: ['progress', 'coverage'],
     port: 9876,
     colors: true,
@@ -50,7 +25,6 @@ module.exports = function (config) {
     browsers: ['Chrome'],
     singleRun: true,
     concurrency: Infinity,
->>>>>>> webpack
     coverageReporter: {
       includeAllSources: true,
       reporters: [
@@ -62,46 +36,5 @@ module.exports = function (config) {
         { type: 'html', dir: 'coverage/' }
       ]
     },
-<<<<<<< HEAD
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-    customLaunchers: {
-      ChromeES6: {
-        base: 'Chrome',
-        flags: ['--javascript-harmony', '--no-sandbox']
-      }
-    },
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['ChromeES6'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  });
-};
-=======
   })
 }
->>>>>>> webpack
