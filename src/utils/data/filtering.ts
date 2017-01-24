e5d4ec525bc485bae3e68157f0bcdd330f39c6d4
0xNacho <nachogarcia91@gmail.com>
2017-01-24 09:26:58 +0100

    Remove unused filtering function

--- src/utils/data/filtering.ts
+++
@@ -1,9 +0,0 @@
-export function filterByPropAndBBox(data: any, prop: any, range: any) {
-  let array = Array();
-  for (let i = 0; i < data.length; i++) {
-    if (data[i][prop] >= range[0] && data[i][prop] <= range[1]) {
-      array.push(data[i]);
-    }
-  }
-  return array;
-}
