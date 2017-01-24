5a44fa3ec9e275912516787b0a1ddd892b73eeea
0xNacho <nachogarcia91@gmail.com>
2017-01-24 09:55:18 +0100

    Add void return type to sortByField

--- src/utils/data/sorting.ts
+++ src/utils/data/sorting.ts
@@ -1,4 +1,4 @@
-export function sortByField (array: Array<any>, field: string){
+export function sortByField (array: Array<any>, field: string) : void {
     array.sort((e1, e2) => {
         var a = e1[field];
         var b = e2[field];
