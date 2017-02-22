c85f2174f4382bd740eb648f3671592f4256a946
Jorge Yagüe París <jorge.yague@treelogic.com>
2017-02-22 13:13:47 +0100

    Improve getAvailableVisualizations()

---
+++ src/charts/index.ts
@@ -0,0 +1,47 @@
+export {
+    default as Chart
+} from './Chart'
+
+export {
+    default as Linechart
+} from './Linechart';
+
+export {
+    default as Barchart
+} from './Barchart';
+
+export {
+    default as Gauge
+} from './Gauge';
+
+export {
+    default as Heatmap
+} from './Heatmap'
+
+export {
+    default as Scatterplot
+} from './Scatterplot';
+
+export {
+    default as Streamgraph
+} from './Streamgraph';
+
+export {
+    default as StackedArea
+} from './StackedArea';
+
+export {
+    default as Swimlane
+} from './Swimlane';
+
+export {
+    default as Sunburst
+} from './Sunburst';
+
+export {
+    default as Network
+} from './Network';
+
+export {
+    default as PieChart
+} from './PieChart';
--- src/core.ts
+++ src/core.ts
@@ -2,7 +2,7 @@ import { Observable } from 'rxjs';
 import Injector from './Injector';
 import StorageService from './services/StorageService';
 import Chart from './charts/Chart'
-
+import * as charts from './charts/'
 
 //Events
 let visibilityChangeSource = Observable.fromEvent(window, 'visibilitychange');
@@ -24,9 +24,23 @@ Injector.register('onResize', resizeSource);
 Injector.register('localStorageService', localStorageService);
 Injector.register('sessionStorageService', sessionStorageService);
 
-/** 
- * Get the list of visualizations available in Proteic.js
- */
+// /** 
+//  * Get the list of visualizations available in Proteic.js
+//  */
 export function getAvailableVisualizations(): String[] {
-    return ["Linechart", "Barchart", "Gauge", "Heatmap", "Scatterplot", "Streamgraph", "StackedArea", "Swimlane", "Sunburst", "Network", "PieChart"];
+
+    let visualizations = new Set<String>();
+
+    for (let property in charts) {
+        if (Object.getPrototypeOf(this[property].prototype).constructor.name === charts.Chart.name) {
+            visualizations.add(this[property].name);
+        }
+    }
+
+    return Array.from(visualizations);
+}
+
+export function getDefaultOptions(visualization: string) {
+    
+    // return  ;
 }
