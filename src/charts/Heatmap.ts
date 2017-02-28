5a5f0507987adac25b07e5ec793fc3b06c73d502
Jorge Yagüe París <jorge.yague@treelogic.com>
2017-02-28 10:47:38 +0100

    Add null filter to Heatmap

--- src/charts/Heatmap.ts
+++ src/charts/Heatmap.ts
@@ -1,7 +1,7 @@
 import Chart from "./Chart";
 import {defaults} from "../utils/defaults/heatmap";
 import SvgStrategyHeatmap from "../svg/strategies/SvgStrategyHeatmap";
-import {copy} from "../utils/functions";
+import {copy, isValuesInObjectKeys} from "../utils/functions";
 
 class Heatmap extends Chart {
 
@@ -15,11 +15,21 @@ class Heatmap extends Chart {
     }
 
     public keepDrawing(datum: any): void {
-        let datumType = datum.constructor;
+        let datumType = datum.constructor,
+            nullValues = this.config.get('nullValues'),
+            keys = [
+                this.config.get('propertyX'),
+                this.config.get('propertyY'),
+                this.config.get('propertyKey')
+            ];
 
         if(datumType === Array) {
+            let filteredDatum = datum.filter(isValuesInObjectKeys(nullValues, keys));
+            console.log(datum);
+            console.log(filteredDatum);
+            console.log('-----------');
             if (this.data) {
-                this.data = this.data.concat(datum);
+                this.data = this.data.concat(filteredDatum);
             } else {
                 this.data = datum;
             }
