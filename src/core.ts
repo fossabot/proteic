4f26a69bdc6a3775f6bf71d113458a6e04e7c1c5
0xNacho <nachogarcia91@gmail.com>
2017-03-09 16:29:06 +0100

    Move utils functions to functions file

--- src/core.ts
+++ src/core.ts
@@ -2,9 +2,7 @@ import { Observable } from 'rxjs';
 import Injector from './Injector';
 import StorageService from './services/StorageService';
 import Chart from './charts/Chart'
-import * as charts from './charts/'
-import * as colors from './utils/colors'
-import * as defaults from './utils/defaults/'
+
 
 //Events
 let visibilityChangeSource = Observable.fromEvent(window, 'visibilitychange');
@@ -26,45 +24,3 @@ Injector.register('onResize', resizeSource);
 Injector.register('localStorageService', localStorageService);
 Injector.register('sessionStorageService', sessionStorageService);
 
-// /** 
-//  * Get the list of visualizations available in Proteic.js
-//  */
-export function getAvailableVisualizations(): String[] {
-    let visualizations = new Set<String>();
-
-    for (let property in charts) {
-        if (typeof property == 'string' && property !== Chart.name) {
-            visualizations.add(property);
-        }
-    }
-
-    return Array.from(visualizations);
-}
-
-export function getDefaultOptions(visualization: string) {
-    if (visualization in defaults) {
-        return (<any>defaults)[visualization];
-    } else {
-        throw new Error('Unrecognised visualization.');
-    }
-}
-
-export function getColorScales() {
-    let colorScales = new Set<String>();
-
-    for (let property in colors) {
-        if (typeof property == 'string') {
-            colorScales.add(property);
-        }
-    }
-
-    return Array.from(colorScales);
-}
-
-export function getColorScale(name: string) {
-    if (name in colors) {
-        return (<any>colors)[name]
-    } else {
-        throw new Error('Unrecognised color scale.');
-    }
-}
