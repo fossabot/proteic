c38df2e291db97cc2271b8740407bca84117816b
Jorge Yagüe París <jorge.yague@treelogic.com>
2017-06-21 13:51:34 +0200

    Fix  legendCells bug

--- src/utils/defaults/heatmap.ts
+++ src/utils/defaults/heatmap.ts
@@ -5,7 +5,8 @@ import StreamingStrategy from '../../charts/enums/StreamingStrategy';
 
 export const defaults: any = {
     selector: '#chart',
-    colorScale: Colors.sequentialVioletCb(), // TODO fix error and use proteic colours
+    colorScale: Colors.sequentialPurpleInterpolated(), // TODO fix error and use proteic colours
+    legendCells: 13,
     xStep: 1,
     yStep: 1,
     //Axes
