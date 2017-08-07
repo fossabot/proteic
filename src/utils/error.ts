3b176e7b4a29ec5e1ced7bf6a3faea214aed2f86
gyeongchan yun <gyeongchan.yun@treelogic.com>
2017-08-07 12:19:47 +0200

    return to original error.ts

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,8 +1,3 @@
 export function throwError(e: any) {
-    let i, message;
-    for (i in e) {
-        message += i + ':' + e[i] +'\n';
-    }
-    alert(message);
     throw new Error('Error: ' + e);
 }
