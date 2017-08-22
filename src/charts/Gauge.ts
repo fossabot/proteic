8dc5591834a74b25c2411aa61877695bf6cf2386
gyeongchan yun <gyeongchan.yun@treelogic.com>
2017-08-22 14:57:08 +0200

    Add pause function in Gauge chart keepdrawing (developing)

--- src/charts/Gauge.ts
+++ src/charts/Gauge.ts
@@ -11,11 +11,31 @@ class Gauge extends Chart {
             userConfig,
             defaults
         );
+
+        this.storedData = [];
     }
 
+    private storedData: any[]; // TODO move to Chart.ts
+
     public keepDrawing(datum: any) {
-        this.data = [datum[0]];
-        super.draw();
+        let pause: boolean = this.config.get('pause');
+
+         if (!pause) {
+             if (this.storedData.length == 0) {
+                 this.data = [datum[0]];
+                 super.draw();
+             } else {
+                this.storedData.push(datum[0]); // To store incoming data when drawing paused data
+
+                for (let i = 0; i < this.storedData.length; i++) {
+                    this.data = [this.storedData.shift()];
+                    super.draw();
+                }
+            }
+        } else {
+            this.storedData.push(datum[0]);
+        }
+
     }
 }
 
