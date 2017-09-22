315a5049d7bdac756b88d2b1d2135dc5c7a2bfc9
gyeongchan yun <gyeongchan.yun@treelogic.com>
2017-09-22 11:26:37 +0200

    Add background line in parallel coordinates

--- src/svg/components/ParallelLineset.ts
+++ src/svg/components/ParallelLineset.ts
@@ -32,26 +32,41 @@ class ParallelLineset extends Component {
 
         let serie = this.svg.select('g.parallelLine').selectAll('g.lineSeries');
         let lines = serie.data(data);
-        
+
         this.elementEnter = lines.enter()
                             .append('g')
                             .attr('class', 'lineSeries')
-                            .attr(Globals.COMPONENT_DATA_KEY_ATTRIBUTE, (d: any) => d[propertyKey])
-                            .append('g')
-                            .attr('class', 'foreground')
-                            .append('svg:path')
-                            .attr('fill', 'none')
-                            .attr('stroke', (d: any, i: number) => legend == true
-                                ? colorScale(d[propertyKey])
-                                : 'steelblue'
-                            )
-                            .style('stroke-width', 1.5)
-                            .attr('d', (d: any) => this.path(d))
-                            .attr('class', 'line');
+                            .attr(Globals.COMPONENT_DATA_KEY_ATTRIBUTE, (d: any) => d[propertyKey]);
+
+        this.elementEnter
+            .append('g')
+            .attr('class', 'background')
+            .append('svg:path')
+            .attr('fill', 'none')
+            .style('stroke-width', 1.5)
+            .attr('stroke', '#ddd')
+            .attr('d', (d: any) => this.path(d))
+            .attr('class', 'back-line');
+
+        this.elementEnter
+            .append('g')
+            .attr('class', 'foreground')
+            .append('svg:path')
+            .attr('fill', 'none')
+            .attr('stroke', (d: any, i: number) => legend == true
+                ? colorScale(d[propertyKey])
+                : 'steelblue'
+            )
+            .style('stroke-width', 1.5)
+            .attr('d', (d: any) => this.path(d))
+            .attr('class', 'front-line');
 
         this.elementExit = lines.exit().remove();
 
-        this.elementUpdate = this.svg.selectAll('.line')
+        this.elementUpdate = this.svg.selectAll('.front-line')
+                                    .data(data)
+                                    .attr('d', (d: any) => this.path(d));
+        this.elementUpdate = this.svg.selectAll('.back-line')
                                     .data(data)
                                     .attr('d', (d: any) => this.path(d));
 
